19ce2de3481b708e0de959099e1e2725205d8f5e
Jesse Williams <jeswill@unc.edu>
2022-08-18 15:29:06 -0500

    Daily blog on coding

--- exercises/playingaround.py
+++ exercises/playingaround.py
@@ -1,1 +1,2 @@
-print("Please work.")
+print("First day of coding set up was a struggle bus. Today was fun after getting help of folder set up.")
+print("Made my first hello world code after getting back from the help I received.")
