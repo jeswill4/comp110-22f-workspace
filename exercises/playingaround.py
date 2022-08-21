dfd027325983d1c5bac789aa60009ba3827fad6e
Jesse Williams <jeswill@unc.edu>
2022-08-21 17:01:05 -0500

    blog

--- exercises/playingaround.py
+++ exercises/playingaround.py
@@ -1,3 +1,4 @@
 print("First day of coding set up was a struggle bus. Today was fun after getting help of folder set up.")
 print("Made my first hello world code after getting back from the help I received.")
 print("Learned about str, int, how to write \"\", bools, float, bits, objects, item, docstring, etc.")
+print("So I learned more in depth about binary and letters and numbers all have a specific type.")
