bd6d1028baae436d0fb950a077c4d62959fb6803
Jesse Williams <jeswill@unc.edu>
2022-08-22 09:38:50 -0500

    fixed ages

--- lessons/ages.py
+++ lessons/ages.py
@@ -3,7 +3,7 @@
 age: int = int(input("How old are you? "))
 year: int = int(input("What year is it? "))
 age_in_2041: int = 2041 - year + age
-print("In 2041, you'll be " + str(age))
+print("In 2041, you'll be " + str(age_in_2041))
 
 """Reassigning values for previously initialized variables"""
 age = age + 1
