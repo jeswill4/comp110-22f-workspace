a536247b54471b78d21616be979455ff8363d383
Jesse Williams <jeswill@unc.edu>
2022-08-29 19:13:56 -0500

    sofarsogood

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,8 +2,16 @@
 __author__ = 730561311
 
 wordle_secret: str = "python"
-wordle_guess: str = (input("What is your 6-letter guess? "))
-i: int = 0 
-while i < len(wordle_guess):
-    print("" + wordle_guess[i])
-    i = i + 1
+wordle_guess: str = input("What is your 6-letter guess? ")
+ 
+while len(wordle_guess) < 6 or len(wordle_guess) > 6: 
+    wordle_guess: str = input ("That is not 6 letters! Try again: ")
+
+if len(wordle_guess) == len(wordle_secret):
+    if wordle_guess == wordle_secret:
+        print("Woo! You got it!") 
+    else:
+        print("Not quite. Play again soon!")
+
+
+
