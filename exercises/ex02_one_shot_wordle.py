da59f5463e8a4a8423c287736c1cfc7446f1819b
Jesse Williams <jeswill@unc.edu>
2022-08-30 15:07:38 -0500

    holy fucking shit big strides were made in draft.

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,25 +2,21 @@
 __author__ = 730561311
 
 wordle_secret: str = "python"
-wordle_guess: str = input("What is your 6-letter guess? ")
+number_of_letters: int = len(wordle_secret)
+wordle_guess: str = input(f"What is your { number_of_letters }-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-letters: int = 0
-letter_block: str = wordle_guess[letters]
-while letters < 6:
-    if letter_block == wordle_secret[letters]:
-        letter_block == GREEN_BOX
-    else:
-        letter_block != wordle_secret[letters]
-        letter_block == WHITE_BOX
-        if letters == 5:
-            print(letter_block(0) + letter_block(1) + letter_block(2) + letter_block(3) + letter_block(4) + letter_block(5))
-    letters = letters + 1
 
-while len(wordle_guess) < 6 or len(wordle_guess) > 6: 
+def l(a: str, b: int) -> str:
+    if a[b] == wordle_secret[b]:
+        return GREEN_BOX
+    else: 
+        return WHITE_BOX
+print(l(wordle_guess, 0) + l(wordle_guess, 1) + l(wordle_guess, 2) + l(wordle_guess, 3) + l(wordle_guess, 4) + l(wordle_guess, 5))
+"""Testing for correct amount of letters in guess to amount of letters in correct wordle."""
+while len(wordle_guess) < len(wordle_secret) or len(wordle_guess) > len(wordle_secret): 
     wordle_guess: str = input ("That is not 6 letters! Try again: ")
-
 while len(wordle_guess) == len(wordle_secret):
     if wordle_guess == wordle_secret:
         print("Woo! You got it!")
