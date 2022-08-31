540726c37204d35f7a8b3ecdb2b13f6596dc734b
Jesse Williams <jeswill@unc.edu>
2022-08-31 09:58:25 -0500

    had it and lost it ;(

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,19 +4,24 @@ __author__ = 730561311
 wordle_secret: str = "python"
 number_of_letters: int = len(wordle_secret)
 wordle_guess: str = input(f"What is your { number_of_letters }-letter guess? ")
+while len(wordle_guess) < len(wordle_secret) or len(wordle_guess) > len(wordle_secret): 
+    wordle_guess: str = input ("That is not 6 letters! Try again: ")
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-while len(wordle_guess) < len(wordle_secret) or len(wordle_guess) > len(wordle_secret): 
-    wordle_guess: str = input ("That is not 6 letters! Try again: ")
-
 def l(a: str, b: int) -> str:
-    if a[b] == wordle_secret[b]:
-        return GREEN_BOX
-    else: 
-        return WHITE_BOX
-print(l(wordle_guess, 0) + l(wordle_guess, 1) + l(wordle_guess, 2) + l(wordle_guess, 3) + l(wordle_guess, 4) + l(wordle_guess, 5))
+    while b < len(wordle_secret):
+        if a[b] == wordle_secret[b]:
+            return GREEN_BOX
+        else: 
+            return WHITE_BOX
+    b = b + 1
+
+print(l(wordle_guess, 0))
+
+
 while len(wordle_guess) == len(wordle_secret):
     if wordle_guess == wordle_secret:
         print("Woo! You got it!")
