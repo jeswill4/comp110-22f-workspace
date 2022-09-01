a75a5b78455c028ec621f72670509f1ce62f7c04
Jesse Williams <jeswill@unc.edu>
2022-08-31 20:27:51 -0500

    finally back where i was

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,21 +6,16 @@ number_of_letters: int = len(wordle_secret)
 wordle_guess: str = input(f"What is your { number_of_letters }-letter guess? ")
 while len(wordle_guess) < len(wordle_secret) or len(wordle_guess) > len(wordle_secret): 
     wordle_guess: str = input ("That is not 6 letters! Try again: ")
-
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-
 def l(a: str, b: int) -> str:
-    while b < len(wordle_secret):
-        if a[b] == wordle_secret[b]:
-            return GREEN_BOX
-        else: 
-            return WHITE_BOX
-    b = b + 1
+    if a[b] == wordle_secret[b]:
+        return GREEN_BOX
+    else:
+        return WHITE_BOX 
 
-print(l(wordle_guess, 0))
-
+print(l(wordle_guess, 0) + l(wordle_guess, 1) + l(wordle_guess, 2) + l(wordle_guess, 3) + l(wordle_guess, 4) + l(wordle_guess, 5))
 
 while len(wordle_guess) == len(wordle_secret):
     if wordle_guess == wordle_secret:
