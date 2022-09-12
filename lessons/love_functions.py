6802e7711e445913ba08479e94308fed8b2b9b1e
Jesse Williams <jeswill@unc.edu>
2022-09-12 09:48:27 -0500

    lets go got it back

--- lessons/love_functions.py
+++ lessons/love_functions.py
@@ -1,15 +1,17 @@
 """Some tender loving functions!"""
 def love(subject: str) -> str:
     """Give a subject as a parameter, returns a loving string."""
-    return f"I love you {subject}"
+    return f"I love you {subject}!"
 
-from lessons.love_functions import love
 def spread_love(to: str, n: int) -> str:
     """Generates a str repeating a loving message n times."""
     love_note: str = ""
     i: int = 0
     while i < n:
-        love_note = love(to) + "\n"
-    i += 1
+        love_note += love(to) + "\n"
+        i += 1
     return love_note
-    
+
+print(spread_love("Whiskers", 4))
+print(spread_love("Moon", 4))
+print(spread_love("you", 4))
