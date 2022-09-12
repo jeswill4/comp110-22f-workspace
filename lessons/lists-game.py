5bf2efda0ecae2581b8eda3e01a36983082e74bd
Jesse Williams <jeswill@unc.edu>
2022-09-12 10:46:05 -0500

    liosts2

--- lessons/lists-game.py
+++ lessons/lists-game.py
@@ -8,6 +8,20 @@ rolls: list[int] = list()
 while len(rolls) == 0 or rolls[len(rolls) - 1] != 1:
     rolls.append(randint(1, 6))
 
+print(rolls)
+# Remove an item from the list by its index ("pop")
+rolls.pop(len(rolls) - 1)
+print(rolls)
+
+# Sum of values of our rolls!
+i: int = 0
+sum: int = 0
+while i < len(rolls):
+    sum = sum + rolls[i]
+    i = i + 1
+
+print(f"Total score: {sum}")
+
 # rolls.append(randint(1, 6))
 # rolls.append(randint(1, 6))
 # rolls.append(randint(1, 6))
