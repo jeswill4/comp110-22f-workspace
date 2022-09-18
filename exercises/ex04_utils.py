4fed966647ec29c4c01ee597e2fb3330616e4126
Jesse Williams <jeswill@unc.edu>
2022-09-18 14:27:57 -0500

    done

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,38 @@
+"""Putting utils to use in exercise."""
+__author__ = "730561311"
+
+
+def all(listy: list[int], searchy: int) -> bool:
+    """Function all used to search number and check to see if list contains only that number. True/False."""
+    i: int = 0 
+    while i < len(listy) and len(listy) > 0:
+        if listy[i] != searchy:
+            return False
+        i += 1
+    return True 
+
+
+def max(input: list[int]) -> int:
+    """Function max returns largest integar in list. If empty return Value Error."""
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    maximum_num: int = 0
+    i: int = 0
+    while i < len(input):
+        if input[i] > maximum_num:
+            maximum_num: input[i]
+        i += 1
+    return maximum_num
+
+
+def is_equal(listy: list[int], input: list[int]) -> bool:
+    """Finding if two lists are equal in each index."""
+    i: int = 0
+    if len(listy) == len(input):
+        while i < len(listy):
+            if listy[i] != input[i]:
+                return False
+            i += 1
+        return True
+    
+    
--- quizprep.py
+++
@@ -1,5 +0,0 @@
-
-a: str = (9//2)
-b: str = (9 % 2)
-c: str = (str(a) + str(" ") + str(b))
-print(c)
