3da2fc442a625c5957d08190336e1a11196f827c
Jesse Williams <jeswill@unc.edu>
2022-09-20 09:25:29 -0500

    confused

--- code_ideas/rock-paper-scissor.py
+++ code_ideas/rock-paper-scissor.py
@@ -1,2 +1,12 @@
 """Rock-paper-scissor game."""
 
+print("ROCK-PAPER-SCISSORS\nTHE GAME")
+username: str = input("Player name: ")
+print(f"{username} vs MACHINE.\n{username} type what you're thinking of - rock/paper/scissor\nThen your pick will shoot against the MACHINE")
+
+def shoot(picks: list[str]) -> str:
+    picks["rock", "paper", "scissor"]
+    
+
+
+def winner(user: str, machine: str) -> str:
