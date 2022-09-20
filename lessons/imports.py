d70924c4f6ad3a24df30ded011c60d3dedbab231
Jesse Williams <jeswill@unc.edu>
2022-09-20 09:12:34 -0500

    did lesson

--- lessons/imports.py
+++ lessons/imports.py
@@ -3,11 +3,19 @@
 
 from lessons import helpers
 
+# Alias a module / imported name as another name
+# simplifies helpers. to hp. when calling function, hp.powerful
+from lessons import helpers as hp
+
+# Import names defined globally in a module
+# no longer need file name. before fn
+from lessons.helpers import powerful, THE_ANSWER
 
 def main() -> None:
     """Entrypoint of program."""
     print(helpers.powerful(2,4))
-
-
+    print(f"The answer: {hp.THE_ANSWER}")
+    print(powerful(2, 4))
+    print(THE_ANSWER)
 if __name__ == "__main__":
     main()
