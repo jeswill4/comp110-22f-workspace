d90f08d2da890839f8ff3ab901d2fb3a8e32c98d
Jesse Williams <jeswill@unc.edu>
2022-09-25 13:28:08 -0500

    corerctions

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,6 +1,7 @@
 """Utils 1 of 2 files in exercise 05."""
 __author__ = "730561311"
 
+
 def only_evens(wholelist: list[int]) -> list[int]:
     """Takes a list of numbers and outputs a newlist of any even numbers from that list."""
     i: int = 0
@@ -11,11 +12,13 @@ def only_evens(wholelist: list[int]) -> list[int]:
         i += 1
     return newlist
 
+
 def concat(firstlist: list[int], secondlist: list[int]) -> list[int]:
     """Given two list this concat function combines them."""
     completelist: list[int] = list()
     completelist.append(firstlist, secondlist)
     return completelist
+
 
 def sub(finallist: list[int], startnum: int, endnum: int) -> list[int]:
     """Generates new list that doesn't have numbers before start and numbers after end."""
--- exercises/ex05/utils_tests.py
+++ exercises/ex05/utils_tests.py
@@ -3,14 +3,17 @@ __author__ = "730561311"
 
 from utils import only_evens, sub, concat
 
+
 def test_list_output_even() -> None:
     """Testing (only_evens) to see if the function when given a list only outputs numbers that are even from that list."""
     mylistex: list[int] = [33, 8, 47, 92, 7, 4, 10]
     assert only_evens(mylistex)
 
+
 def test_if_startnumber_is_negative_one() -> None:
     """Testin sub to see if it creates a new list from given, that starts and ends with given numbers."""
     assert sub([8, 42, 4, 10], -1, 3)
+
 
 def test_concat_list_with_list() -> None:
     """Testing to combine two lists together."""
