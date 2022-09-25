6d593658da29d6daaf1853247ccf28e0112b53bb
Jesse Williams <jeswill@unc.edu>
2022-09-25 14:36:38 -0500

    ...

--- code_ideas/rock-paper-scissor.py
+++ code_ideas/rock-paper-scissor.py
@@ -3,41 +3,33 @@
 print("ROCK-PAPER-SCISSORS\nTHE GAME")
 username: str = input("Player name: ")
 print(f"{username} vs MACHINE.\n{username}, type what you're thinking of - ROCK/PAPER/SCISSORS\nThen your pick will shoot against the MACHINE")
-picks: list[str] = ["ROCK","PAPER","SCISSORS"] 
+picks: list[str] = ["ROCK", "PAPER", "SCISSORS"]
 
 
 def picked(user: str) -> int:
-    if user == "ROCK":
+    if user == picks[0]:
         return 0
-    if user == "PAPER":
+    if user == picks[1]:
         return 1
-    if user == "SCISSOR":
+    if user == picks[2]:
         return 2
-
-def strpicked(usernum: int) -> str:
-    if usernum == 0:
-        return "ROCK"
-    if usernum == 1:
-        return "PAPER"
-    if usernum == 2:
-        return "SCISSORS"
 
 def numtostr(machine: int) -> str:
     if machine == 0:
-        return "ROCK"
+        return picks[0]
     if machine == 1:
-        return "PAPER"
+        return picks[1]
     if machine == 2:
-        return "SCISSORS"
+        return picks[2]
 
 def winner(urnum: int, compnumb: int) -> str:
-    print(f"MACHINE SHOOTS {numtostr(compnumb)}! <--> {username} SHOOTS {strpicked(urnum)}!")
+    print(f"MACHINE SHOOTS {numtostr(compnumb)}! <--> {username} SHOOTS {numtostr(urnum)}!")
     computernumber: int = compnumb
     while picked == computernumber:
         print("AGAIN")
         picked(input("Thinking of ROCK or PAPER or SCISSORS: "))
         computernumber: int = randint(0,2)
-        print(f"MACHINE SHOOTS {numtostr(computernumber)}! <--> {username} SHOOTS {strpicked(picked)}!")
+        print(f"MACHINE SHOOTS {numtostr(computernumber)}! <--> {username} SHOOTS {numtostr(picked)}!")
 
     if (picked == 0 and computernumber == 2) or (picked == 1 and computernumber == 0) or (picked == 2 and computernumber == 1):
         print(f"{username} WON!!!! ")
@@ -47,7 +39,7 @@ def winner(urnum: int, compnumb: int) -> str:
 
 picked(input("What are you thinking? ROCK, PAPER, or SCISSORS? Type exactly the same way, all capatilized letters: "))
 from random import randint
-computernumber: int = randint(0,2)
+computernumber: int = randint(0, 2)
 winner(picked, computernumber)
 
     
