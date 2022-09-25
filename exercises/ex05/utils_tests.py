fc49c1cf5f72c97eeb63eff99e15eca62606c69e
Jesse Williams <jeswill@unc.edu>
2022-09-25 12:56:25 -0500

    done?

--- exercises/ex05/utils_tests.py
+++ exercises/ex05/utils_tests.py
@@ -3,11 +3,17 @@ __author__ = "730561311"
 
 from utils import only_evens, sub, concat
 
-def test_only_evens() -> None:
-    """descriptive docstring"""
+def test_list_output_even() -> None:
+    """Testing (only_evens) to see if the function when given a list only outputs numbers that are even from that list."""
+    mylistex: list[int] = [33, 8, 47, 92, 7, 4, 10]
+    assert only_evens(mylistex)
 
-def test_sub() -> None:
-    """descriptive docstrings"""
+def test_if_startnumber_is_negative_one() -> None:
+    """Testin sub to see if it creates a new list from given, that starts and ends with given numbers."""
+    assert sub([8, 42, 4, 10], -1, 3)
 
-def test_concat() -> None:
-    """descriptive docstringss"""
+def test_concat_list_with_list() -> None:
+    """Testing to combine two lists together."""
+    unolist: list[int] = [4, 7]
+    doslist: list[int] = [10, 18]
+    assert concat(unolist, doslist)
