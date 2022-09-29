1ce826956a58fa0d01f86f3572dcd24d40ef698e
Jesse Williams <jeswill@unc.edu>
2022-09-29 12:23:43 -0500

    finished lesson on sequences.

--- lessons/sequences.py
+++ lessons/sequences.py
@@ -2,9 +2,13 @@
 
 # Declaring a type alias that "invents" the Point2d type
 Point2D = tuple[float, float]
+# Camel casing, no underscoring and capitalizing starting letters of words
+# Traced outline of word it looks like a camel back
+# Gives us hint that type was made by us not python type
 
 start_position: Point2D = (5.0, 10.0)
+print(start_position)
 # can't change individual items in it, not like a list that can be popped or appended
-start_posiiton = (start_position[0] + 5.0, start_position[1] + 10.0)
+start_position = (start_position[0] + 5.0, start_position[1] + 10.0)
 print(start_position)
 
