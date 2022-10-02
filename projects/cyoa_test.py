8aac65c95d624a7b4c596237e62e7ae12bdfc5c5
Jesse Williams <jeswill@unc.edu>
2022-10-02 08:40:09 -0500

    yep exercises

--- projects/cyoa_test.py
+++
@@ -1,7 +0,0 @@
-"""Testing rock paper scissors."""
-
-from projects.cyoa import achievement_tracked, winner_award, player
-
-def test_achievements() -> None:
-    winner_award() == True
-    assert achievement_tracked() == print(f"\nACHIEVEMENT - {player} DOMINATION\n Win +5 times or more in a row")
