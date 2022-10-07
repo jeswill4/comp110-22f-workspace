b99057ed5a95d3bca4b544e8afe0769667f18f71
Jesse Williams <jeswill@unc.edu>
2022-10-07 15:58:01 -0400

    done?

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -6,23 +6,21 @@ __author__ = "730561311"
 def invert(inversion: dict[str, str]) -> dict[str,str]:
     """Invert should swap key with values."""
     for key in inversion: 
-        og_key = key
-        og_value = inversion(key)
-        inversion(key) += og_key
-        inversion(key) -= og_value
-        key += og_value
-        key -= og_key
+        og_value = inversion[key]
+        inversion[key] = key
+        key = og_value
 
 
 def favorite_colors(colors: dict[str, str]) -> str:
     """Names, favorite color. Returns most popular color. If tie return 1st to appear color of the tie."""
     color_list: list[str] = list()
     color_quantity: list[int] = list()
+    set_highest: str = ""
     for key in colors:
-        color = colors(key)
-        color_list.append(color)
+        color_list.append(colors(key))
     i: int = 0
     m: int = 1 
+    n: int = 0
     while i < len(color_list):
         color_amount: int = 1
         while m < len(color_list):
@@ -31,8 +29,6 @@ def favorite_colors(colors: dict[str, str]) -> str:
             m += 1
         color_quantity.append(color_amount)
         i += 1
-    n: int = 0
-    set_highest: str = ""
     while n < len(color_quantity):
         set_num: int = 0
         if color_quantity[n] > set_num:
@@ -46,8 +42,9 @@ def count(count_dracula: list[str]) -> dict[str, int]:
     """Count produces dictionary of a list with [list str, count of # times that str appeared in list]."""
     empty_dict: dict[str, int]
     empty_dict = {}
-    
-
-
-
-
+    for index in count_dracula:
+        if count_dracula[index] in empty_dict:
+            empty_dict[count_dracula[index]] += 1
+        else:
+            empty_dict += {count_dracula[index]: 1}
+    return empty_dict
