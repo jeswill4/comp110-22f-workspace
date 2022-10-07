a8f8cfa0da49d9c9aaccf1ea8392209e00e4b12f
Jesse Williams <jeswill@unc.edu>
2022-10-07 15:07:11 -0400

    major progress

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -3,11 +3,48 @@
 __author__ = "730561311"
 
 
-invert
+def invert(inversion: dict[str,str]) -> dict[str,str]:
+    """Invert should swap key with values."""
+    for key in inversion: 
+        og_key = key
+        og_value = inversion(key)
+        inversion(key) += og_key
+        inversion(key) -= og_value
+        key += og_value
+        key -= og_key
 
-count
 
-favorite_colors
+def favorite_colors(colors: dict[str,str]) -> str:
+    """Names, favorite color. Returns most popular color. If tie return 1st to appear color of the tie."""
+    color_list: list[str] = list()
+    color_quantity: list[int] = list()
+    for key in colors:
+        color = colors(key)
+        color_list.append(color)
+    i: int = 0
+    m: int = 1 
+    while i < len(color_list):
+        color_amount: int = 1
+        while m < len(color_list):
+            if color_list[i] == color_list[m]:
+                color_amount += 1
+            m += 1
+        color_quantity.append(color_amount)
+        i += 1
+    n: int = 0
+    set_highest: str = ""
+    while n < len(color_quantity):
+        set_num: int = 0
+        if color_quantity[n] > set_num:
+            set_num = color_quantity[n]
+            set_highest = color_list[n]
+        n += 1
+    return set_highest 
+    
+
+
+
+count: dict[,]
 
 
 
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -2,8 +2,21 @@
 
 __author__ = "730561311"
 
-test_
+from dictionary import invert, count, favorite_colors
 
-test_
+def test_invert_calender(month: str, day: str) -> None:
+    """Inverting month with day """
+    my_dictionary = {"December": "25th", "April": "4th", "October": "31st"}
+    invert(my_dictionary)
 
-test_
+
+def test_favorite_colors_tie() -> None:
+    """Test tie situation of favorite color counts. Should return first color mentioned in the tie."""
+    my_dictionary = {"Jesse": "orange", "Albert": "orange", "Miguel": "blue", "DJ" : "blue"}
+    favorite_colors(my_dictionary)
+
+
+def test_count_nothing() -> None:
+    """Using count defintion but with no inputs."""
+    my_dictionary = {}
+    count(my_dictionary)
