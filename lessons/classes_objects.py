db893eb49b47db116f8f3ee8892b4d9bc5a1019f
Jesse Williams <jeswill@unc.edu>
2022-10-25 16:29:52 -0400

    done

--- lessons/classes_objects.py
+++ lessons/classes_objects.py
@@ -1,28 +1,36 @@
 """Example of a class and object instantiation."""
 
+from xmlrpc.server import SimpleXMLRPCDispatcher
+
+
 class Pizza: 
     """Models the idea of a Pizza."""
     # Attribute Definitions
-    size: str
-    toppings: int
-    extra_cheese: bool
+    size: str 
+    toppings: int 
+    extra_cheese: bool = False
 
-def price(self: Pizza) -> float:
-    """Calculate the price of a Pizza."""
-    total: float = 0.0
-    if self.size == "large":
-        total += 10.0 
-    else: 
-        total += 8.0 
-    total += self.toppings * 0.75
-    if self.extra_cheese:
-        total += 1.0
-    return total
+    def __init__(self, size: str, toppings: int):
+        """Constructor definition for initialization of attributes."""
+        self.size = size
+        self.toppings = toppings
 
+    def price(self, tax: float) -> float:
+        """Calculate the price of a Pizza."""
+        total: float = 0.0
+        if self.size == "large":
+            total += 10.0 
+        else: 
+            total += 8.0 
+        total += self.toppings * 0.75
+        if self.extra_cheese:
+            total += 1.0
+        total += tax
+        return total
 
+a_pizza: Pizza = Pizza("large", 3)
+print(f"Large pizza, 3 toppings - Price: ${a_pizza.price(1.05)}")
 
-a_pizza: Pizza = Pizza()
-a_pizza.size = "large"
-a_pizza.toppings = 3
-a_pizza.extra_cheese = False
-print(f"Price: ${price(a_pizza.price())}")
+another_pizza: Pizza = Pizza("small", 0)
+another_pizza.extra_cheese = True
+print(f"Small pizza, 0 toppings, extra cheese - Price: ${another_pizza.price(1.05)}")
