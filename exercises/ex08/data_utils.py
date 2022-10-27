9304cf9b94829ecfac3eded49710ea9ffdbab279
Jesse Williams <jeswill@unc.edu>
2022-10-26 20:43:59 -0400

    vjkviu

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -4,3 +4,15 @@ __author__ = "730561311"
 
 # Define your functions below
 
+from io import TextIOWrapper
+
+
+def read_csv_rows(entirety: str) -> list[dict[str,str]]:
+    """Reads entirety of CSV data and puts in into a list of rows."""
+    data_rows: list[dict[str, str]] = []
+    file_handle: TextIOWrapper = open(entirety, "r")
+    for data_rows in file_handle:
+        data_rows = data_rows.lower()
+        data_rows = data_rows.strip()
+        data_rows.append(data_rows)
+    return data_rows
