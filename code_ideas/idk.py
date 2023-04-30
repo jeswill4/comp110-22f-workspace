e5e218d5e47ce1334547267b040fc763f6715c57
Jesse Williams <jeswill@unc.edu>
2023-04-30 13:28:58 -0400

    fjeopjv

--- code_ideas/idk.py
+++ code_ideas/idk.py
@@ -1,6 +1,6 @@
 """FIRE STORM"""
 __author__ = "Gummybear"
-game: int = 1
+game: int = 2
 
 """Stats"""
 hp: int = 100
@@ -18,7 +18,8 @@ def fight(hp: int, damage: int) -> int:
     new_hp = (hp - damage)
     return new_hp
 
-def introduction(hp: int, fire: int, wind:int, duckhp:int, duckfire: int, duckwind: int, exp: int) -> None:
+def introduction() -> None:
+    """Intro"""
     print(" ")
     print(" ")
     print("                  _______________")
@@ -38,9 +39,9 @@ def introduction(hp: int, fire: int, wind:int, duckhp:int, duckfire: int, duckwi
     print("Hp represents health, fire is your attack damage, wind is your speed.")
     print("If your wind is slower than your enemy's wind, the enemy attacks first.")
     print("If your health gets below 1 you die. You will be able to add to your stats after leveling up!")
-    attacks("duck", hp, fire, wind, duckhp, duckfire, duckwind, exp)
 
-def attacks(attakers_name: str, hp: int, fire: int, wind: int, duckhp: int, duckfire: int, duckwind:int, exp: int) -> None:
+def attacks(attakers_name: str, hp: int, fire: int, wind: int, duckhp: int, duckfire: int, duckwind:int, exp: int) -> int:
+    """Getting attacked"""
     print(" ")
     print(" ")
     print(f"               Hp: {hp}   \t Fire: {fire} \t Wind: {wind}")
@@ -64,39 +65,81 @@ def attacks(attakers_name: str, hp: int, fire: int, wind: int, duckhp: int, duck
                     exit()
                 duckhp = fight(duckhp, fire)
                 print(f"duck hp: {duckhp}")
-                if duckhp == 0:
+                if duckhp < 0:
                     print(attakers_name, " defeated")
                     exp += 1
+                    return exp
             else:
                 print("You attack first")
                 duckhp = fight(duckhp, fire)
                 print(f"duck hp: {duckhp}")
-                if duckhp == 0:
+                if duckhp < 0:
                     print(attakers_name, " defeated")
                     exp += 1
+                    return exp
                 hp = fight(hp, duckfire)
                 print(f"hp: {hp}")
                 if hp < 1:
                     print("Hp = 0 \tGame Over")
                     exit()
-        if exp > 0:
-            print("Level Up!")
-            up: str = input("Increase \"hp\", \"fire\", or \"wind\":    ")
-            if up == hp:
-                hp += 10
+
+def lvl(exp: int, hp: int, fire: int, wind: int) -> int:
+    if exp > 0:
+        print(" ")
+        print(" ")
+        print("                    Level Up!")
+        up: str = input("Increase \"hp\", \"fire\", or \"wind\":    ")
+        if up == "hp":
+            hp += 10
             print("Hp +10")
-            if up == fire:
-                fire += 2
-                print("Fire +2")
-            if up == wind:
-                wind += 1
-                print("Wind +1")
-            exp = 0 
+            print(" ")
+            print(" ")
+            return hp
+        if up == "fire":
+            fire += 2
+            print("Fire +2")
+            print(" ")
+            print(" ")
+            return fire
+        if up == "wind":
+            wind += 1
+            print("Wind +1")
+            print(" ")
+            print(" ")
+            return fire
+    return 0
+
+introduction()
+exp = attacks("duck", hp, fire, wind, duckhp, duckfire, duckwind, exp)
+levelup = lvl(exp, hp, fire, wind)
+if levelup == (hp + 10):
+    hp += 10
+    exp = 0
+if levelup == (fire + 2):
+    fire += 2
+    exp = 0
+if levelup == (wind + 1):
+    wind += 1
+    exp = 0
+print("V    You're good at beating up afflack! Embrace the inferno and go deeper    V")
+print(f"Exp: {exp} Hp: {hp} Fire: {fire} Wind: {wind}")
 for round in range(game):
-    introduction(hp, fire, wind, duckhp, duckfire, duckwind, exp)
-    print("V    You're good at beating up afflack! Embrace the inferno and go deeper    V")
-    continues: str = input("Would you like to continue or save? Type \"c\" or \"s\"")
-    if continues == "c":
-        hp = attacks("duck", hp, fire, wind, duckhp, duckfire, duckwind, exp)
-    if continues == "s":
-        print("Saved")
+    answer = input("Do you want to continue? \"y\" for yes and \"n\" for no:    ")
+    while answer != "y" or "n":
+        print("You typed your answer wrong. Try again.")
+        answer = input("Do you want to continue? \"y\" for yes and \"n\" for no:    ")
+    if answer == "y":
+        exp = attacks("duck", hp, fire, wind, duckhp, duckfire, duckwind, exp)
+        levelup = lvl(exp, hp, fire, wind)
+        if levelup == (hp + 10):
+            hp += 10
+            exp = 0
+        if levelup == (fire + 2):
+            fire += 2
+            exp = 0
+        if levelup == (wind + 1):
+            wind += 1
+            exp = 0
+        print(f"Exp: {exp} Hp: {hp} Fire: {fire} Wind: {wind}")
+    if answer == "n":
+        exit()
