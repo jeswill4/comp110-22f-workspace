0cd2d7bc48627289df05ae9dce7b864f4269c8d8
Jesse Williams <jeswill@unc.edu>
2023-04-29 21:07:03 -0400

    yesahofdne

--- code_ideas/idk.py
+++ code_ideas/idk.py
@@ -1,6 +1,5 @@
-"""Make a game."""
+"""FIRE STORM"""
 __author__ = "Gummybear"
-
 game: int = 1
 
 """Stats"""
@@ -12,44 +11,68 @@ wind: int = 1
 duckhp: int = 25
 duckfire: int = 4
 duckwind: int = 2
-def duck(what_needed: str) -> int:
-    if what_needed == "hp":
+
+def duck(stat: str) -> int:
+    """Gives stat desired"""
+    if stat == "hp":
         return duckhp
-    if what_needed == "fire":
+    if stat == "fire":
         return duckfire
-    if what_needed == "wind":
+    if stat == "wind":
         return duckwind
 
+def fight(hp: int, damage: int) -> int:
+    """When someone takes damage, gives new HP."""
+    new_hp = (hp - damage)
+    return new_hp
 
-"""Intro"""
-print("Fire Storm")
-print("Hp represents health, fire is your attack damage, wind is your speed. If your wind is slower than your enemy's wind, the enemy attacks first. If your health gets below 1 you die. You will be able to add to your stats after leveling up!")
+def introduction(hp: int, fire: int, wind:int) -> None:
+    print("Fire Storm")
+    print("Hp represents health, fire is your attack damage, wind is your speed. If your wind is slower than your enemy's wind, the enemy attacks first. If your health gets below 1 you die. You will be able to add to your stats after leveling up!")
+    print(f"Hp: {hp} \t Fire: {fire} \t Wind: {wind} ")
+    attacks("duck", hp, fire, wind)
 
-print(f"Hp: {hp} \t Fire: {fire} \t Wind: {wind} ")
-print("Duck appears")
+def attacks(attakers_name: str, hp: int, fire: int, wind: int) -> int:
 
-
-
-
-def attacks(attakers_name) -> str:
-    attack: str = input("Attack with \"x\"")
-    while attack != "x":
-        print("You typed \"x\" wrong")
+    print(f"{attakers_name} appears")
+    if attakers_name == "duck":
+        print("Hp:", duck("hp"), "Fire: ", duck("fire"), "Wind:", duck("wind"))
         attack: str = input("Attack with \"x\"")
-    if attack == "x":
-        if attakers_name("wind") > wind:
-            print(f"{attakers_name} attacks first")
-            hp -= attakers_name("fire")
-            if hp < 1:
-                print("Hp = 0 \tGame Over")
-                exit()
-        else:
-            print("You attack first")
-            attackers_hp = attakers_name("hp") - fire
-
+        while attack != "x":
+            print("You typed \"x\" wrong")
+            attack: str = input("Attack with \"x\"")
+        if attack == "x":
+            while duck("hp") > 0:
+                if duck("wind") > wind:
+                    print(f"{attakers_name} attacks first")
+                    hp = fight(hp, duck("fire"))
+                    print(hp)
+                    if hp < 1:
+                        print("Hp = 0 \tGame Over")
+                        exit()
+                    duckhp = fight(duck("hp"), fire)
+                    print(duckhp)
+                    if duckhp == 0:
+                        print(attakers_name, " defeated")
+                        return hp 
+                else:
+                    print("You attack first")
+                    duckhp = fight(duck("hp"), fire)
+                    print(duckhp)
+                    if duckhp == 0:
+                        print(attakers_name, " defeated")
+                        return hp
+                    hp = fight(hp, duck("fire"))
+                    print(hp)
+                    if hp < 1:
+                        print("Hp = 0 \tGame Over")
+                        exit()
 
 for round in range(game):
-    print(f"Hp: {hp} \t Fire: {fire} \t Wind: {wind} ")
-    continues: str = input("Would you like to continue or save? Type \"yes\" or \"no\"")
-    if continues == "yes":
-        print("yeah")
+    introduction(hp, fire, wind)
+    hp = attacks("duck", hp, fire, wind)
+    continues: str = input("Would you like to continue or save? Type \"c\" or \"s\"")
+    if continues == "c":
+        hp = attacks("duck", hp, fire, wind)
+    if continues == "s":
+        print("Saved")
